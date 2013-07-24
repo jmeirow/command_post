50ad245baf37cdc1a3207b59c905f31c7c0bd050
Joe Meirow <joe.meirow@gmail.com>
2013-07-24 04:55:26 -0400

    cleaned up spacing

--- db/postgresql.sql
+++ db/postgresql.sql
@@ -1,33 +1,41 @@
-
 --drop table aggregate_events;
-
 --drop table aggregates;
 
 
 
 create table aggregate_events (
+
     transaction_id      bigint primary key,  
     aggregate_id        bigint,  
     aggregate_type      varchar(100),
     event_description   text,
     content             text,
     call_stack          text, 
-    user_id         varchar(100) not null,
+    user_id         	varchar(100) not null,
     transacted          timestamp
+
    )  ;
+
+create index aggregate_id_idx on aggregate_events(aggregate_id,aggregate_type);
+
+
+
+
   
 create table aggregates (
-aggregate_id    bigint not null primary key,
+
+aggregate_id    		bigint not null primary key,
 aggregate_lookup_value  varchar(100) not null,
-aggregate_type    varchar(100) not null,
-content         text not null
+aggregate_type    		varchar(100) not null,
+content         		text not null
+
 )  ; 
-
-
 
 create index aggregate_lookup_idx on aggregates(aggregate_lookup_value);
 
-create index aggregate_id_idx on aggregate_events(aggregate_id,aggregate_type);
+
+
+
   
 
 CREATE SEQUENCE aggregate START 1;
