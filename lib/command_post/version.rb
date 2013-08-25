29f8cf881bcaf3148e29e04c63e89e589ddcbbe8
Joe Meirow <joe.meirow@gmail.com>
2013-08-25 16:59:13 -0400

    bump version number

--- lib/command_post/version.rb
+++ lib/command_post/version.rb
@@ -1,3 +1,9 @@
 module CommandPost
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
+
+# 1. bump version number in this file
+# 2. run the command below from the command line.
+# $ gem build command_post.gemspec
+# $ gem build command_post-0.0.5.gem
+
