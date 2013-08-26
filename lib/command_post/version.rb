99c6d3f8cd7e06b985451fe9a4e3b62bad5af1d9
Joe Meirow <joe.meirow@gmail.com>
2013-08-25 21:59:02 -0400

    fixed important comment in version.rb

--- lib/command_post/version.rb
+++ lib/command_post/version.rb
@@ -5,5 +5,5 @@ end
 # 1. bump version number in this file
 # 2. run the command below from the command line.
 # $ gem build command_post.gemspec
-# $ gem build command_post-0.0.6.gem
+# $ gem push command_post-0.0.6.gem
 
