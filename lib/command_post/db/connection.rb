f9fc919dfdb588691d10a62107712a133820f706
Darren Cauthon <darren@cauthon.com>
2013-12-02 20:32:20 -0600

    Do what I need to get the app running locally.

--- lib/command_post/db/connection.rb
+++ lib/command_post/db/connection.rb
@@ -3,8 +3,13 @@ module CommandPost
 
   class Connection
 
+    class << self
+      attr_accessor :connection_string
+    end
+
     def self.db_cqrs
-      Sequel.connect("postgres://localhost/cqrs?user=postgres&password=#{ENV['password']}")
+      cs = connection_string || "postgres://localhost/cqrs?user=postgres&password=#{ENV['password']}"
+      Sequel.connect(cs)
     end
 
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require File.expand_path(File.dirname(__FILE__) + '/../lib/command_post/db/connection')
+CommandPost::Connection.connection_string = ENV['CONNECTION_STRING']
+
 require File.expand_path(File.dirname(__FILE__) + '/../lib/command_post')
 require 'minitest/autorun'
 require 'minitest/spec'
